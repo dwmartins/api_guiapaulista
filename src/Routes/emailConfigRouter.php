da63e465836b1c080e51d2f4513cdd0415f5f461
Douglas Martins <douglas5422@outlook.com>
2024-07-02 21:40:30 -0300

    feat: Add middleware nas rotas de config de e-mail

--- src/Routes/emailConfigRouter.php
+++ src/Routes/emailConfigRouter.php
@@ -1,6 +1,11 @@
 <?php
 
 use App\Http\Route;
+use App\Middleware\UserMiddleware;
 
-Route::post('/emailconfig', 'EmailConfigController@create');
-Route::get('/emailconfig', 'EmailConfigController@fetch');
+Route::post('/emailconfig', 'EmailConfigController@create', [
+    [UserMiddleware::class, 'isAuth']
+]);
+Route::get('/emailconfig', 'EmailConfigController@fetch', [
+    [UserMiddleware::class, 'isAuth']
+]);
